cb4b75baffcc1257608e1d69cc1164f1d9e12564
Javid R <reddypallij@gmail.com>
2025-04-16 22:54:51 +0530

    Added code

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/run")
+def run():
+    cmd = request.args.get("cmd")
+    os.system(cmd)  # Insecure code: command injection
+    return "Done"
